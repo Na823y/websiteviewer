4608cc971ba81ba19f44f09fa70c1327f77f6c25
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-04-08 21:40:38 +0900

    Create websiteviewer.js

---
+++ websiteviewer.js
@@ -0,0 +1,10 @@
+document.querySelector("html").innerHTML = `
+  <head>
+    <meta charset="UTF-8">
+    <title>webさいとびゅーあ build-2024.04.08-bata</title>
+    <link rel="stylesheet" href="https://naochan83275.github.io/websiteviewer/viewer-client.css">
+  </head>
+  <body>
+    <iframe src="https://naochan83275.github.io/websiteviewer/websiteviewer.html" frameborder="0"></iframe>
+  </body>
+`
