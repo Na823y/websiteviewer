6e2f85c1c3c70c06d03bdf7a765a607464b54e44
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-10-29 21:46:22 +0900

    Update websiteviewer.js

--- websiteviewer.js
+++ websiteviewer.js
@@ -3,8 +3,9 @@ document.querySelector("html").innerHTML = `
     <meta charset="UTF-8">
     <title>webさいとびゅーあ build-2024.04.08-bata</title>
     <link rel="stylesheet" href="https://naochan83275.github.io/websiteviewer/viewer-client.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
-    <iframe src="https://naochan83275.github.io/websiteviewer/websiteviewer.html" frameborder="0" width="100%" height="100%"></iframe>
+    <iframe src="https://naochan83275.github.io/websiteviewer/websiteviewer.html" frameborder="0"></iframe>
   </body>
 `
