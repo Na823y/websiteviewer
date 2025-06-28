8dcf276fd4967619d04f0d9e6c903cd0ab624cea
Na823 <166379976+Na823y@users.noreply.github.com>
2025-06-28 14:33:46 +0900

    URL変わったので対応

--- websiteviewer.js
+++ websiteviewer.js
@@ -2,10 +2,10 @@ document.querySelector("html").innerHTML = `
   <head>
     <meta charset="UTF-8">
     <title>webさいとびゅーあ build-2024.04.08-bata</title>
-    <link rel="stylesheet" href="https://naochan83275.github.io/websiteviewer/viewer-client.css">
+    <link rel="stylesheet" href="https://na823y.github.io/websiteviewer/viewer-client.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
-    <iframe src="https://naochan83275.github.io/websiteviewer/websiteviewer.html" frameborder="0"></iframe>
+    <iframe src="https://na823y.github.io/websiteviewer/websiteviewer.html" frameborder="0"></iframe>
   </body>
 `
